e3bb63df8bffab5d33ee8b8fa2fac6540c4ed75d
designartt <designarttdev@gmail.com>
2019-11-13 13:08:21 -0300

    Diretivas

--- Diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ Diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  cursos: string[] = ["Angular 2"];
+  cursos: string[] = ['Angular 2'];
 
-  mostrarCrusos: boolean = false;
+  mostrarCrusos = false;
 
 
 
---
+++ Diretivas/src/app/teste/teste.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.scss']
+})
+export class TesteComponent implements OnInit {
+
+
+minhaVariavel: string;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
